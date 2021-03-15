ff4335d148e745c3d7130ac27698cf340b89d88a
csouter14 <71992661+csouter14@users.noreply.github.com>
2021-03-15 17:07:19 -0600

    19.2

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,18 @@
 // Create a variable below
+var helloWorld = 'Hello world';
 
 // Console.log() your variable here.
+console.log(helloWorld);
+
+var kidsinhouse =3;
+var kidsoutside=2;
+
+var x = '3';
+var y = '2';
+var z = 'There are' + (x + y) + 'kids at my house';
+
+function addkids () {
+alert (kidsinhouse + kidsoutside)
+}
+
+addkids ()
