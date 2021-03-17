123b3e445043d6a43da8612f678ba8378e01c92e
csouter14 <71992661+csouter14@users.noreply.github.com>
2021-03-17 16:53:14 -0600

    19.3

--- 19.2-activities/01_Conditionals/unsolved/js/index.js
+++ 19.2-activities/01_Conditionals/unsolved/js/index.js
@@ -1,3 +1,19 @@
 $('.tab').on("click", function(){
-  
+ 
+  if ($(this).hasclass("tab1")) {
+    $(".tab1").addclass("active");
+    $(".tab2").removeclass("active");
+    $("#tabcontent1").css("display", "block");
+    $("#tabcontent2").css("display", "none");
+    console.log("tab 1 was clicked");
+
+  }
+  else if ($(this).hasclass("tab1")){
+    $(".tab2").addclass("active");
+    $(".tab1").removeclass("active");
+    $("#tabcontent2").css("display", "block");
+    $("#tabcontent1").css("display", "none");
+    console.log("tab 2 was clicked")
+  }
+
 });
